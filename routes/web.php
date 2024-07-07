e1c4f1afb4b12553836ee54c83d7075c12198aa1
Saqlain <itssaqlain06@gmail.com>
2024-07-07 17:10:07 +0500

    Worked on validation and authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Http\Controllers\AcountController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+Route::group(['prefix' => 'account'], function () {
+
+    Route::group(['middleware' => 'guest'], function () {
+
+        Route::get('register', [AcountController::class, 'register'])->name('account.register');
+        Route::post('register', [AcountController::class, 'processRegister'])->name('account.processRegister');
+
+        Route::get('login', [AcountController::class, 'login'])->name('account.login');
+        Route::post('login', [AcountController::class, 'authenticate'])->name('account.authenticate');
+    });
+
+    Route::group(['middleware' => 'auth'], function () {
+        Route::get('profile', [AcountController::class, 'profile'])->name('account.profile');
+        Route::get('logout', [AcountController::class, 'logout'])->name('account.logout');
+        Route::post('update-profile', [AcountController::class, 'updateProfile'])->name('account.updateProfile');
+    });
 });
