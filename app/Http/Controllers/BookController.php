fc1a57406351fa9e2b8c576cc8ffd904655f114e
Saqlain <itssaqlain06@gmail.com>
2024-07-10 22:20:51 +0500

    Working of CRUD on books

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,20 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BookController extends Controller
 {
     public function index()
     {
+        return view('books.list');
     }
 
     public function create()
     {
+        return view('books.create');
     }
 
-    public function store()
+    public function store(Request $request)
     {
+        $rules = [
+            'title' => 'required|min:5',
+            'author' => 'required|min:5',
+            'value' => 'required'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+
+        if (!empty($request->image)) {
+            $rules['image'] = 'image';
+        }
+        if ($validator->fails()) {
+            return redirect()->route('books.create')->withInput()->withErrors($validator);
+        }
+
+        $book = new Book();
+        $book->title = $request->title;
+        $book->author = $request->author;
+        $book->description = $request->description;
+        $book->status = $request->status;
+        $book->save();
+
+        return redirect()->route('books.index')->with('success', 'Book added successfully.');
     }
 
     public function edit()
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AcountController;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -24,5 +25,9 @@ Route::group(['prefix' => 'account'], function () {
         Route::get('profile', [AcountController::class, 'profile'])->name('account.profile');
         Route::get('logout', [AcountController::class, 'logout'])->name('account.logout');
         Route::post('update-profile', [AcountController::class, 'updateProfile'])->name('account.updateProfile');
+
+        Route::get('books', [BookController::class, 'index'])->name('books.index');
+        Route::get('books/create', [BookController::class, 'create'])->name('books.create');
+        Route::post('books/store', [BookController::class, 'store'])->name('books.store');
     });
 });
